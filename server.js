cc0fc3b33f8009671fa65241f93fc4983000a643
Muhamad <melassar2000@gmail.com>
2020-06-25 14:14:01 -0400

    added templates and router

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const {createServer} = require('http');
+const { createServer } = require('http');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -9,25 +9,25 @@ const port = process.env.PORT || 8080;
 
 const dev = app.get('env') !== 'production';
 
-if(!dev) {
+if (!dev) {
     app.disable('x-powered-by');
     app.use(compression());
     app.use(morgan('common'));
 
     app.use(express.static(path.resolve(__dirname, 'build')))
 
-    app.get('*', function(req, res) {
+    app.get('*', function (req, res) {
         res.sendFile(path.resolve(__dirname, 'build', '/index.html'));
     });
 }
 
-if(dev) {
+if (dev) {
     app.use(morgan('dev'));
 }
 
 const server = createServer(app);
-server.listen(port, err=> {
-    if(err) throw err;
+server.listen(port, err => {
+    if (err) throw err;
 
     console.log("Server started!");
 })
--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HomepageLayout } from './Home';
+import { LoginForm } from './Login'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={() => <HomepageLayout />} />
+        <Route exact path='/login' component={() => <LoginForm />} />
+      </Switch>
+    </Router>
   );
 }
 
