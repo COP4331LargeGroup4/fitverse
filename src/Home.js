ac3e1494f39b6f3d7d04be8d391819ea7eb7e243
jkraftchick <54610324+jkraftchick@users.noreply.github.com>
2020-06-25 16:39:20 -0400

    added links to login and signup buttons

--- src/Home.js
+++ src/Home.js
@@ -103,8 +103,8 @@ class DesktopContainer extends Component {
                                 <Menu.Item as='a'>Company</Menu.Item>
                                 <Menu.Item as='a'>Careers</Menu.Item>
                                 <Menu.Item position='right'>
-                                    <Button as='a' inverted={!fixed} href="/login">Log in</Button>
-                                    <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
+                                    <Button as='a' inverted={!fixed} href="/signin">Log in</Button>
+                                    <Button as='a' inverted={!fixed} href="/signup" primary={fixed} style={{ marginLeft: '0.5em' }}>
                                         Sign Up
                                     </Button>
                                 </Menu.Item>
