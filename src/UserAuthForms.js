6646095f2bb3bf0a7e9bf7e458a7a60e6d2bf6e8
jkraftchick <54610324+jkraftchick@users.noreply.github.com>
2020-06-27 17:55:17 -0400

    updated reset password page to use formik

--- src/UserAuthForms.js
+++ src/UserAuthForms.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
+//import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
@@ -11,7 +11,13 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { Img } from 'react-image';
 import Logo from "./logo.svg";
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import { EmailValidator } from 'email-validator';
+import { Formik, Form, Field } from 'formik';
+import { LinearProgress } from '@material-ui/core';
+import { TextField } from 'formik-material-ui';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { green } from '@material-ui/core/colors';
 
 function Copyright() {
 	return (
@@ -27,6 +33,14 @@ function Copyright() {
 }
 
 const useStyles = makeStyles((theme) => ({
+	root: {
+		display: 'flex',
+		alignItems: 'center',
+	  },
+	  wrapper: {
+		margin: theme.spacing(1),
+		position: 'relative',
+	  },
 	paper: {
 		marginTop: theme.spacing(4),
 		display: 'flex',
@@ -40,7 +54,197 @@ const useStyles = makeStyles((theme) => ({
 	submit: {
 		margin: theme.spacing(3, 0, 2),
 	},
+	buttonProgress: {
+		position: 'absolute',
+		top:'50%',
+		left:'50%',
+		marginRight:'-50%',
+		transform:'translate(-50%,-50%)'
+	},
 }));
+
+
+export function ResetPassword() {
+	const classes = useStyles();
+
+	return (
+		<div style={{ backgroundColor: '#D9DBF1', height: '100vh', paddingTop: 48 }}>
+			<Container component="main" maxWidth="xs" justify="center" style={{ backgroundColor: '#FFFFFF', padding: 24, borderRadius: 24, marginTop: 48, border: '3px solid #ACB0BD' }}>
+				<CssBaseline />
+				<div className={classes.paper}>
+					<NavLink exact to="/">
+						<Img src={Logo} style={{ maxWidth: "100%" }} />
+					</NavLink>
+					<Typography component="h1" variant="h5" style={{ marginTop: 20 }}>
+						Forgot your password?
+					</Typography>
+					<Typography component="subtitle1">
+						Enter your email to reset it
+					</Typography>
+
+					<Formik
+						initialValues={{
+							email: '',
+						}}
+						validate={values => {
+							const errors: Partial<Values> = {};
+							if (!values.email) {
+								errors.email = 'Required';
+							} else if (
+								!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+								//!EmailValidator.validate(values.email)
+							) {
+								errors.email = 'Invalid email address';
+							}
+							return errors;
+						}}
+						onSubmit={(values, { setSubmitting }) => {
+							
+							setTimeout(() => {
+								setSubmitting(false);
+								alert(JSON.stringify(values));
+							}, 500);
+						}}
+					>
+						{({ submitForm, isSubmitting }) => (
+							<Form className={classes.form}>
+								<Field
+									component={TextField}
+									name="email"
+									type="email"
+									label="Email"
+									required
+									fullWidth
+									variant="outlined"
+									margin="normal"
+								/>
+								<Button
+									fullWidth
+									variant="contained"
+									color="primary"
+									disabled={isSubmitting}
+									onClick={submitForm}
+									className={classes.submit}
+								>
+									Send password reset email
+									{isSubmitting && <CircularProgress size={12} className={classes.buttonProgress} />}
+    							</Button>
+								
+							</Form>
+						)}
+					</Formik>
+				</div>
+				<Box mt={8}>
+					<Copyright />
+				</Box>
+			</Container>
+		</div>
+	);
+}
+
+
+
+/*<Formik
+						initialValues={{
+							email: '',
+						}}
+						validate={values => {
+							const errors: Partial<Values> = {};
+							if (!values.email) {
+								errors.email = 'Required';
+							} else if (!EmailValidator.validate(values.email)) {
+								errors.email = 'Invalid email address';
+							}
+							return errors;
+						}}
+						onSubmit={(values, { setSubmitting }) => {
+							setTimeout(() => {
+								setSubmitting(false);
+								alert(values);
+							}, 500);
+						}}
+					>
+						{({ submitForm, isSubmitting }) => (
+							<Form>
+
+								<Field
+									component={TextField}
+									name="email"
+									type="email"
+									label="Email"
+									required
+									fullWidth
+									variant="outlined"
+									margin="normal"
+								/>
+								{isSubmitting}
+								<Button
+									type="submit"
+									fullWidth
+									variant="contained"
+									color="primary"
+									className={classes.submit}
+								>
+									Send password reset email
+								</Button>
+							</Form>
+						)}
+					</Formik>*/
+
+
+
+/*export function ResetPassword() {
+	const classes = useStyles();
+
+	return (
+		<div style={{ backgroundColor: '#D9DBF1', height: '100vh', paddingTop: 48 }}>
+			<Container component="main" maxWidth="xs" justify="center" style={{ backgroundColor: '#FFFFFF', padding: 24, borderRadius: 24, marginTop: 48, border: '3px solid #ACB0BD' }}>
+				<CssBaseline />
+				<div className={classes.paper}>
+					<NavLink exact to="/">
+						<Img src={Logo} style={{ maxWidth: "100%" }} />
+					</NavLink>
+					<Typography component="h1" variant="h5" style={{ marginTop: 20 }}>
+						Forgot your password?
+					</Typography>
+					<Typography component="subtitle1">
+						Enter your email to reset it
+					</Typography>
+					<form className={classes.form} noValidate>
+						<TextField
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="Email Address"
+							name="email"
+							autoComplete="email"
+							autoFocus
+						/>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							color="primary"
+							className={classes.submit}
+						>
+							Send password reset email
+						</Button>
+					</form>
+				</div>
+				<Box mt={8}>
+					<Copyright />
+				</Box>
+			</Container>
+		</div>
+	);
+}*/
+
+
+
+
+
 
 export function SignUp() {
 	const classes = useStyles();
@@ -199,54 +403,6 @@ export function SignIn() {
 								</NavLink>
 							</Grid>
 						</Grid>
-					</form>
-				</div>
-				<Box mt={8}>
-					<Copyright />
-				</Box>
-			</Container>
-		</div>
-	);
-}
-
-export function ResetPassword() {
-	const classes = useStyles();
-
-	return (
-		<div style={{ backgroundColor: '#D9DBF1', height: '100vh', paddingTop: 48 }}>
-			<Container component="main" maxWidth="xs" justify="center" style={{ backgroundColor: '#FFFFFF', padding: 24, borderRadius: 24, marginTop: 48, border: '3px solid #ACB0BD' }}>
-				<CssBaseline />
-				<div className={classes.paper}>
-					<NavLink exact to="/">
-						<Img src={Logo} style={{ maxWidth: "100%" }} />
-					</NavLink>
-					<Typography component="h1" variant="h5" style={{ marginTop: 20 }}>
-						Forgot your password?
-					</Typography>
-					<Typography component="subtitle1">
-						Enter your email to reset it
-					</Typography>
-					<form className={classes.form} noValidate>
-						<TextField
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							id="email"
-							label="Email Address"
-							name="email"
-							autoComplete="email"
-							autoFocus
-						/>
-						<Button
-							type="submit"
-							fullWidth
-							variant="contained"
-							color="primary"
-							className={classes.submit}
-						>
-							Send password reset email
-						</Button>
 					</form>
 				</div>
 				<Box mt={8}>
