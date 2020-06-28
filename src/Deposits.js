56c6f932a47374df754e9d39d12c0866086c16a6
Vanillacruz <54645562+Vanillacruz@users.noreply.github.com>
2020-06-27 20:16:07 -0400

    changed button color
    still haven't been able to get the button to span the the length of the grid

--- src/Deposits.js
+++ src/Deposits.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Title from './Title';
-import { ButtonGroup } from '@material-ui/core';
-import { ButtonToolBar } from '@material-ui/core';
 import { Button } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import { green, purple } from '@material-ui/core/colors';
 
 function preventDefault(event) {
   event.preventDefault();
@@ -22,14 +22,24 @@ export default function Deposits() {
   return (
     <React.Fragment>
       <Title>Hello</Title>
-      <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
-        <Button>My Goals</Button>{' '}
-        <Button>View Progress</Button>{' '}
-      </ButtonGroup>
-      <ButtonGroup color="primary" aria-label="outlined primary button group">
-        <Button>Add Workout</Button>
-        <Button>View/Edit Workouts</Button>
-      </ButtonGroup>
+      <Grid container spacing={3}>
+            {/* Chart */}
+            <Grid item lg={6}>
+            <Button size="large" variant="contained" color="primary"  style={{backgroundColor: '#416164'}} block>My Goals</Button>
+            </Grid>
+            {/* Recent Deposits */}
+            <Grid item lg={6}>
+            <Button size="large" variant="contained" color="primary"  style={{backgroundColor: '#416164'}}>View Progress</Button>
+            </Grid>
+            {/* Recent Orders */}
+            <Grid item lg={6}>
+            <Button size="large" variant="contained" color="primary"  style={{backgroundColor: '#416164'}}>Add Workout</Button>
+            </Grid>
+            <Grid item lg={6}>
+            <Button size="large" variant="contained" color="primary"  style={{backgroundColor: '#416164'}}>View / Edit Workouts</Button>
+            </Grid>
+          </Grid>
+        
     </React.Fragment>
   );
 }
