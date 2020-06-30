4adce98eba927ea7f7a67fbafe19efba0b4f22d6
O'Neal Thomas <43864397+onealthomas61@users.noreply.github.com>
2020-06-29 21:00:04 -0400

    Update User.model.js

--- Models/User.model.js
+++ Models/User.model.js
@@ -2,12 +2,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    Login: {
-            Name: String,
-            Email: String,
-            Username: String,
-            Password: String,
-           }
+    Name:{
+        type: String,
+        required: true
+    },
+    
+    Email:{
+        type: String,
+        required: true
+    },
+    
+    Username:{
+        type: String,
+        required: true
+    },
+    
+    Password:{
+        type: String,
+        required: true}
 });
 
 const User =  mongoose.model("Users", UserSchema);
