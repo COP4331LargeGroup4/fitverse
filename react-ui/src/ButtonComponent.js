629b1543a356e378edaba11f63df0476ba27bfad
Vanillacruz <54645562+Vanillacruz@users.noreply.github.com>
2020-07-05 16:43:35 -0400

    Add Workout Dialog Updates
    added a goals text-box
    
    tooks Muhamad's repeat SMTW.. buttons
    Still need to fix some styling

--- react-ui/src/ButtonComponent.js
+++ react-ui/src/ButtonComponent.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import React, { useState, useEffect } from 'react';
+import {
+  Container,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  TextField,
+  Button,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Select,
+  ListSubheader,
+  IconButton,
+  InputLabel,
+  FormControl,
+  FormControlLabel,
+  Switch,
+  makeStyles,
+} from '@material-ui/core';
 import Title from './Title';
-import { Button } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
-import { NavLink } from 'react-router-dom'
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import 'date-fns';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-
-function preventDefault(event) {
-  event.preventDefault();
-}
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab'
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -44,9 +55,13 @@ const useStyles = makeStyles((theme) => ({
   selectEmpty: {
     marginTop: theme.spacing(2),
   },
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
 }));
-
-
 
 export default function ButtonComponents() {
   const classes = useStyles();
@@ -73,6 +88,42 @@ export default function ButtonComponents() {
     });
   };
 
+  const handleRepeatToggle = () => {
+    toggleWeekDayPicker(!weekdayPicker);
+  }
+
+  const handleChangeRepeatedDay = (event, newSelected) => {
+    let newState = Object.assign({}, currentEvent);
+    newState.repeat = newSelected;
+    setCurrentEvent(newState);
+  }
+
+  const [currentEvent, setCurrentEvent] = useState(
+  {
+    title: '',
+    startDate: '',
+    endDate: '',
+    repeat: []
+  });
+
+  const [weekdayPicker, toggleWeekDayPicker] = useState(false);
+
+  const DayPicker = () => {
+    var daysOfWeek = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+    return (
+        <ToggleButtonGroup value={currentEvent.repeat} onChange={handleChangeRepeatedDay}>
+            {daysOfWeek.map((day, key) => (
+                <ToggleButton
+                    key={key}
+                    value={key}
+                >
+                    {day}
+                </ToggleButton>
+
+            ))}
+        </ToggleButtonGroup>
+    )
+}
 
   const AddWorkoutDialog = () =>{
     return(
@@ -93,50 +144,62 @@ export default function ButtonComponents() {
             id="workoutName"
             label="Workout Name"
           />
-          <TextField
-              id="StartDate"
-              label="Select a Start Date"
-              type="date"
-              required
-              margin="dense"
-              defaultValue="2020-06-30"
-              className={classes.textField}
-              InputLabelProps={{
-                shrink: true,
-              }}
+          <Container maxWidth="lg" className={classes.container} style={{padding: 0, marginTop: 5}}>
+            <Grid container spacing={3}>
+              <Grid item>
+                <TextField
+                    id="StartDate"
+                    label="Select a Start Date"
+                    type="date"
+                    required
+                    margin="dense"
+                    defaultValue="2020-06-30"
+                    className={classes.textField}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                    id="EndDate"
+                    label="Select an End Date"
+                    type="date"
+                    margin="dense"
+                    defaultValue="2020-06-30"
+                    className={classes.textField}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                />
+              </Grid>
+            </Grid>
+          </Container>
+          <FormControlLabel
+            style={{ marginLeft: 0 }}
+            control={<Switch color="primary" onChange={handleRepeatToggle} checked={weekdayPicker} />}
+            labelPlacement="start"
+            label="Repeat weekly"
           />
+            
+          {weekdayPicker &&
+          <div>
+            <DayPicker />
+          </div>
+          }
           <TextField
-              id="EndDate"
-              label="Select an End Date"
-              type="date"
-              margin="dense"
-              defaultValue="2020-06-30"
-              className={classes.textField}
-              InputLabelProps={{
-                shrink: true,
-              }}
+            margin="dense"
+            marginTop="2"
+            name="myGoals"
+            variant="outlined"
+            fullWidth
+            type="goals"
+            id="outlined-multiline-static"
+            label="My Goals for this Workout"
+            multiline
+            rows={4}    
           />
-  
-        <FormControl className={classes.formControl}>
-          <InputLabel shrink  htmlFor="weekDayMonth">Repeat Every</InputLabel>
-          <Select
-            native
-            defaultValue="hi"
-            value={state.numAmount}
-            onChange={handleChange}
-            inputProps={{
-              name: 'numAmount',
-              id: 'weekDayMonth',
-            }}
-          >
-            <option aria-label="week" value="" />
-            <option value={10}>week</option>
-            <option value={20}>month</option>
-            <option value={30}>year</option>
-          </Select>
-      </FormControl>
-          
-          
+
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
