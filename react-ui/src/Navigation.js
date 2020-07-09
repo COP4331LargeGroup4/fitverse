b59b66372560653115979cea8316f6107165290e
Vanillacruz <54645562+Vanillacruz@users.noreply.github.com>
2020-07-09 10:13:54 -0400

    removed bell notification icon from dashboard

--- react-ui/src/Navigation.js
+++ react-ui/src/Navigation.js
@@ -9,10 +9,8 @@ import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 
 const drawerWidth = 240;
 
@@ -132,11 +130,6 @@ export function Navigation(props) {
                     <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
                         {props.title}
                     </Typography>
-                    <IconButton color="inherit">
-                        <Badge badgeContent={4} color="secondary">
-                            <NotificationsIcon />
-                        </Badge>
-                    </IconButton>
                 </Toolbar>
             </AppBar>
             <Drawer
