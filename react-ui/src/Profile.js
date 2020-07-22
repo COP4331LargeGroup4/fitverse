88da86cf5c05f571d2f6cb99338f0b55cbf8bdf4
Vanillacruz <54645562+Vanillacruz@users.noreply.github.com>
2020-07-21 22:45:35 -0400

    removed outline and added boxShadow to match other components

--- react-ui/src/Profile.js
+++ react-ui/src/Profile.js
@@ -167,7 +167,7 @@ function Profile() {
     
 	return (
 		<div style={{ height: '100vh', paddingTop: 48, alignItems: 'center', width:"100%" }}>
-			<Container component="main" maxWidth="xs" justify="center" style={{ backgroundColor: '#D9DBF1', padding: 20, borderRadius: 24, marginTop: 48, border: '3px solid #ACB0BD' }}>
+			<Container component="main" maxWidth="xs" justify="center" style={{ backgroundColor: '#D9DBF1', padding: 20, borderRadius: 24, marginTop: 48, boxShadow: '3px 3px 10px 6px #ccc' }}>
 				<CssBaseline />
 				<div className={ classes.paper } style={{ backgroundColor: '#D9DBF1'}}>
 					{success == null ? <Form /> : <Success />}
